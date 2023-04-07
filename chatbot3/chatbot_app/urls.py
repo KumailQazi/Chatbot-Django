d79e7be86486bd2aff18afbad6efc2067063d0c6
KumailQazi <tag.kumail@gmail.com>
2023-04-08 03:32:09 +0500

    updates

--- chatbot3/chatbot_app/urls.py
+++ chatbot3/chatbot_app/urls.py
@@ -1,6 +1,6 @@
 # new url file created
 from django.urls import path, include
-from chatbot_app.views import chat
+from chatbot_app.views import chat, chatbot, chat2, chat3
 
 # from . import views
 
@@ -9,6 +9,9 @@ app_name = 'chatbot_app'
 urlpatterns = [
     # path('', views.home, name='home'),
     path('chat/', chat, name='chat'),
+    path('chatbot/', chatbot, name='chatbot'),
+    path('chat2/', chat2, name='chat2'),
+    path('chat3/', chat3, name='chat3'),
     # path('chatbot_app/', include('chatbot_app.urls')),
     # other paths here
 ]
